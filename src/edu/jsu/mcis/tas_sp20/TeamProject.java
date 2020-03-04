02dd76ad31023eee4185250cffc3d7bdc93270cd
caitl <caitl@10.80.30.52>
2020-03-04 11:53:21 -0600

    Test commit

--- src/edu/jsu/mcis/tas_sp20/TeamProject.java
+++ src/edu/jsu/mcis/tas_sp20/TeamProject.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.jsu.mcis.tas_sp20;
 
-/**
- *
- * @author NLammonds
- */
 public class TeamProject {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }

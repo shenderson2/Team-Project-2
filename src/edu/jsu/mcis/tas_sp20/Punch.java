62041f8b97ec004791127632b415b7e06ec1f102
Justin Thomas <jthomas54@stu.jsu.edu>
2020-03-04 19:14:01 -0600

    Punch Class

--- src/edu/jsu/mcis/tas_sp20/Punch.java
+++ src/edu/jsu/mcis/tas_sp20/Punch.java
@@ -4,40 +4,68 @@
  * and open the template in the editor.
  */
 package edu.jsu.mcis.tas_sp20;
-
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Punch {
   private String adjustmenttype;
   private int terminalid;
   private int punchtypeid;
   private static int id;
-  private String originaltimestamp;
+  private long originaltimestamp;
   Badge badge;
+  private GregorianCalendar gc = new GregorianCalendar ();
+  
           
-  public Punch(Badge badge, int terminalid, int punchtypeid){
+  public Punch(int terminalid, Badge badge, long originaltimestamp, int punchtypeid){
         this.punchtypeid = punchtypeid;
         this.terminalid = terminalid;
+        this.originaltimestamp = originaltimestamp;
+        this.badge = badge;
   }
-   public Badge getbadge(){
-     return badge;
- }
    public Punch(int id){
        Punch.id = id;
    }
+     public Badge getbadge(){
+     return badge;
+ }
     public int getid(){
         return id;
     }
    public int getterminalid(){
       return terminalid;
   }
-   public String get_originaltimestamp(){
-      return originaltimestamp; 
+   public long getoriginaltimestamp(){
+        return originaltimestamp; 
    }
-   public void set_orginaltimestamp(String timestamp){
-        this.originaltimestamp = timestamp;
-    }
    public int getpunchtypeid(){
       return punchtypeid;
   }
-
+   public void setOriginalTimeStamp(long ts){
+       gc.setTimeInMillis(originaltimestamp);
+       this.originaltimestamp = ts;
+   }
+    
+    public String printOriginalTimestamp(){
+      StringBuilder sb = new StringBuilder("#");
+      sb.append(badge);
+      switch(punchtypeid){
+          case 0:
+            sb.append(" CLOCKED OUT:");
+            break;
+          case 1:
+            sb.append(" CLOCKED IN:");
+            break; 
+          case 2:
+            sb.append(" TIMED OUT:");
+            break;
+          default:
+            sb.append(" ERROR");
+      }
+       sb.append(originaltimestamp);
+       
+       SimpleDateFormat sdf = new SimpleDateFormat("EEE MM/dd/yyyy HH:mm:ss");
+       sb.append(sdf.format(gc.getTime()).toUpperCase());
+       return sb.toString();
+    }
 }

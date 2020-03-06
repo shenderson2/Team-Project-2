8d3fb33ae5a3bd1b7f64618cf9f7f42c2a950eba
Caitlin1798 <caitlinhatchel@yahoo.com>
2020-03-06 11:34:19 -0600

    Completed shift 2

--- src/edu/jsu/mcis/tas_sp20/Shift.java
+++ src/edu/jsu/mcis/tas_sp20/Shift.java
@@ -1,48 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.jsu.mcis.tas_sp20;
 
 import java.sql.Time;
 
-/**
- *
- * @author caitl
- */
+
 public class Shift {
        
         public int shiftId;
+        
+        public String description;
 
         public Time start;
 
         public Time stop;
         
-        public int lunchduration;
+        public int interval;
+
+        public int graceperiod;
+
+        public int dock;
 
         public Time lunchstart;
 
         public Time lunchstop;
 
-  
+        public int lunchdeduct;
     
 
-        public Shift(int shiftId, Time Start, Time Stop,int Lunchduration, Time Lunchstart, Time Lunchstop){
-
+        public Shift(int ShiftId, String Description, Time Start, Time Stop, int Interval, int Graceperiod, int Dock, Time Lunchstart, Time Lunchstop, int Lunchdeduct){
 
 
             this.shiftId = shiftId;
+            
+            this.description = Description;
 
             this.start = Start;
 
             this.stop = Stop;
 
-            this.lunchduration =Lunchduration;
+            this.interval = Interval; 
+            
+            this.graceperiod = Graceperiod;
+            
+            this.dock = Dock;
             
             this.lunchstart = Lunchstart;
 
             this.lunchstop = Lunchstop;
+            
+            this.lunchdeduct = Lunchdeduct;
 
         }
 

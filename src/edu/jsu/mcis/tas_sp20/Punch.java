134f537ab0dbd16d9696fd743b196acf0597b457
Caitlin1798 <caitlinhatchel@yahoo.com>
2020-04-18 21:01:42 -0500

    Feature 3 Complete

--- src/edu/jsu/mcis/tas_sp20/Punch.java
+++ src/edu/jsu/mcis/tas_sp20/Punch.java
@@ -16,15 +16,14 @@ public class Punch {
     private long originaltimestamp, adjustedtimestamp;
     private Badge badgeid;
     private GregorianCalendar gc = new GregorianCalendar ();
-  
+    private long adjustedtime = 0;
+    private String adjusttype = ""; 
           
     public Punch(int terminalid, Badge badgeid, long originaltimestamp, int punchtypeid){
           this.punchtypeid = punchtypeid;
           this.terminalid = terminalid;
           this.originaltimestamp = originaltimestamp;
           
-
-      //  this.originaltimestamp = System.currentTimeMillis();
           this.badgeid = badgeid;
     }
     public Punch( Badge badgeid, int terminalid, int punchtypeid){
@@ -34,151 +33,148 @@ public class Punch {
           this.originaltimestamp = System.currentTimeMillis();
     } 
     
+    
     public void adjust(Shift s){
-            GregorianCalendar shiftstarttimestamp = new GregorianCalendar();   
-            shiftstarttimestamp.setTimeInMillis(originaltimestamp);
-            shiftstarttimestamp.set(Calendar.HOUR_OF_DAY, s.getstartHour());
-            shiftstarttimestamp.set(Calendar.MINUTE, s.getstartMinute());
-            shiftstarttimestamp.set(Calendar.SECOND, 0);
-            long shiftstart = shiftstarttimestamp.getTimeInMillis();
-            
-            GregorianCalendar shiftstoptimestamp = new GregorianCalendar();   
-            shiftstoptimestamp.setTimeInMillis(originaltimestamp);
-            shiftstoptimestamp.set(Calendar.HOUR_OF_DAY, s.getstopHour());
-            shiftstoptimestamp.set(Calendar.MINUTE, s.getstopMinute());
-            shiftstoptimestamp.set(Calendar.SECOND, 0);
-            long shiftstop = shiftstoptimestamp.getTimeInMillis();
-            
-            GregorianCalendar shiftlstarttimestamp = new GregorianCalendar();   
-            shiftlstarttimestamp.setTimeInMillis(originaltimestamp);
-            shiftlstarttimestamp.set(Calendar.HOUR_OF_DAY, s.getlstartHour());
-            shiftlstarttimestamp.set(Calendar.MINUTE, s.getlstartMinute());
-            shiftlstarttimestamp.set(Calendar.SECOND, 0);
-            long shiftlstart = shiftstarttimestamp.getTimeInMillis();
-            
-            GregorianCalendar shiftlstoptimestamp = new GregorianCalendar();   
-            shiftlstoptimestamp.setTimeInMillis(originaltimestamp);
-            shiftlstoptimestamp.set(Calendar.HOUR_OF_DAY, s.getlstopHour());
-            shiftlstoptimestamp.set(Calendar.MINUTE, s.getlstopMinute());
-            shiftlstoptimestamp.set(Calendar.SECOND, 0);
-            long shiftlstop = shiftstoptimestamp.getTimeInMillis();
-            
-            GregorianCalendar gracestarttimestamp = new GregorianCalendar();
-            gracestarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
-            gracestarttimestamp.add(Calendar.MINUTE, s.getGraceperiod());
-            long gracestart = gracestarttimestamp.getTimeInMillis();
-            
-            GregorianCalendar gracestoptimestamp = new GregorianCalendar();
-            gracestoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
-            gracestoptimestamp.add(Calendar.MINUTE, -(s.getGraceperiod()));
-            long gracestop = gracestoptimestamp.getTimeInMillis();
-            
-            GregorianCalendar Intervalstarttimestamp = new GregorianCalendar();
-            Intervalstarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
-            Intervalstarttimestamp.add(Calendar.MINUTE, s.getInterval());
-            long Intervalstart = Intervalstarttimestamp.getTimeInMillis();
-            
-            GregorianCalendar Intervalstoptimestamp = new GregorianCalendar();
-            Intervalstoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
-            Intervalstoptimestamp.add(Calendar.MINUTE, -(s.getInterval()));
-            long Intervalstop = Intervalstoptimestamp.getTimeInMillis();
-            
-            GregorianCalendar Dockstarttimestamp = new GregorianCalendar();
-            Dockstarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
-            Dockstarttimestamp.add(Calendar.MINUTE, s.getDock());
-            long Dockstart = Dockstarttimestamp.getTimeInMillis();
-            
-            GregorianCalendar Dockstoptimestamp = new GregorianCalendar();
-            Dockstoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
-            Dockstoptimestamp.add(Calendar.MINUTE, -(s.getDock()));
-            long Dockstop = Dockstoptimestamp.getTimeInMillis();
-          
-  
-            switch (this.getPunchtypeid()){
-                case 0:
-            {
-                long punchTime = 0;
-                if ((punchTime <= shiftlstop)
-                        && (punchTime >= shiftlstart)){
-                    this.setAdjustedTimestamp(shiftlstart);
-                    this.setAdjustmenttype("Lunch Start");
-                }
-                if  ((punchTime <= shiftlstop)
-                    && (punchTime >= shiftlstart)){
-                        this.setAdjustedTimestamp(shiftlstop);
-                        this.setAdjustmenttype("Lunch Stop");
-                }
-                
-                //Shift Start and Stop
-                if ((punchTime <= shiftstart)){
-                    this.setAdjustedTimestamp(shiftstart);
-                    this.setAdjustmenttype("Shift Start");
-                }
-                if ((punchTime >= shiftstop)){
-                    this.setAdjustedTimestamp(shiftstop);
-                    this.setAdjustmenttype("Shift Stop");
-                }
-                
-                //Grace period 
-                if ((punchTime > shiftstart) &&
-                        (punchTime <= gracestart)){
-                    this.setAdjustedTimestamp(shiftstart);
-                    this.setAdjustmenttype("Shift Start");
-                }
-                if ((punchTime < shiftstop) &&
-                        (punchTime >= gracestop )){
-                    this.setAdjustedTimestamp(shiftstop);
-                    this.setAdjustmenttype("Shift Stop");
-                }
-                
-                     //Interval Round 
-                if ((punchTime > Dockstart) &&
-                        (punchTime <= Intervalstart)){
-                    this.setAdjustedTimestamp(Intervalstart);
-                    this.setAdjustmenttype("Interval Round");
-                }
-                if ((punchTime < Dockstop) &&
-                        (punchTime >= Intervalstop )){
-                    this.setAdjustedTimestamp(Intervalstop);
-                    this.setAdjustmenttype("Interval Round");
-                }
+        GregorianCalendar originalpunch= new GregorianCalendar();
+        originalpunch.setTimeInMillis(this.originaltimestamp);
+        originalpunch.set(GregorianCalendar.SECOND,0);
+        long ogTime = originalpunch.getTimeInMillis(); 
+        int dayofWeek = originalpunch.get(Calendar.DAY_OF_WEEK);
 
-                    //NON-SPECIAL CLOCK-IN CASES
-                    //Default adjustments
+        GregorianCalendar shiftstarttimestamp = new GregorianCalendar();   
+        shiftstarttimestamp.setTimeInMillis(originaltimestamp);
+        shiftstarttimestamp.set(Calendar.HOUR_OF_DAY, s.getstartHour());
+        shiftstarttimestamp.set(Calendar.MINUTE, s.getstartMinute());
+        shiftstarttimestamp.set(Calendar.SECOND, 0);
+        long shiftstart = shiftstarttimestamp.getTimeInMillis();
+
+        GregorianCalendar shiftstoptimestamp = new GregorianCalendar();   
+        shiftstoptimestamp.setTimeInMillis(originaltimestamp);
+        shiftstoptimestamp.set(Calendar.HOUR_OF_DAY, s.getstopHour());
+        shiftstoptimestamp.set(Calendar.MINUTE, s.getstopMinute());
+        shiftstoptimestamp.set(Calendar.SECOND, 0);
+        long shiftstop = shiftstoptimestamp.getTimeInMillis();
+
+        GregorianCalendar shiftlstarttimestamp = new GregorianCalendar();   
+        shiftlstarttimestamp.setTimeInMillis(originaltimestamp);
+        shiftlstarttimestamp.set(Calendar.HOUR_OF_DAY, s.getlstartHour());
+        shiftlstarttimestamp.set(Calendar.MINUTE, s.getlstartMinute());
+        shiftlstarttimestamp.set(Calendar.SECOND, 0);
+        long shiftlstart = shiftlstarttimestamp.getTimeInMillis();
+
+        GregorianCalendar shiftlstoptimestamp = new GregorianCalendar();   
+        shiftlstoptimestamp.setTimeInMillis(originaltimestamp);
+        shiftlstoptimestamp.set(Calendar.HOUR_OF_DAY, s.getlstopHour());
+        shiftlstoptimestamp.set(Calendar.MINUTE, s.getlstopMinute());
+        shiftlstoptimestamp.set(Calendar.SECOND, 0);
+        long shiftlstop = shiftlstoptimestamp.getTimeInMillis();
+
+        GregorianCalendar gracestarttimestamp = new GregorianCalendar();
+        gracestarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
+        gracestarttimestamp.add(Calendar.MINUTE, s.getGraceperiod());
+        long gracestart = gracestarttimestamp.getTimeInMillis();
+
+        GregorianCalendar gracestoptimestamp = new GregorianCalendar();
+        gracestoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
+        gracestoptimestamp.add(Calendar.MINUTE, -(s.getGraceperiod()));
+        long gracestop = gracestoptimestamp.getTimeInMillis();
+
+        GregorianCalendar Intervalstarttimestamp = new GregorianCalendar();
+        Intervalstarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
+        Intervalstarttimestamp.add(Calendar.MINUTE, s.getInterval());
+        long Intervalstart = Intervalstarttimestamp.getTimeInMillis();
+
+        GregorianCalendar Intervalstoptimestamp = new GregorianCalendar();
+        Intervalstoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
+        Intervalstoptimestamp.add(Calendar.MINUTE, (s.getInterval()));
+        long Intervalstop = Intervalstoptimestamp.getTimeInMillis();
+
+        GregorianCalendar Dockstarttimestamp = new GregorianCalendar();
+        Dockstarttimestamp.setTimeInMillis(shiftstarttimestamp.getTimeInMillis());
+        Dockstarttimestamp.add(Calendar.MINUTE, s.getDock());
+        long Dockstart = Dockstarttimestamp.getTimeInMillis();
+
+        GregorianCalendar Dockstoptimestamp = new GregorianCalendar();
+        Dockstoptimestamp.setTimeInMillis(shiftstoptimestamp.getTimeInMillis());
+        Dockstoptimestamp.add(Calendar.MINUTE, -(s.getDock()));
+        long Dockstop = Dockstoptimestamp.getTimeInMillis();
+
+
+        if (dayofWeek != Calendar.SATURDAY && dayofWeek != Calendar.SUNDAY){ 
+
+            switch (this.getPunchtypeid()){
+                
+                case 0:
+                    
+                    if ((ogTime >= shiftstop) && (ogTime <= Intervalstop)){
+                        adjustedtime = shiftstop;
+                        adjusttype = "Shift Stop";
+                    }
+                    else if ((ogTime <= shiftlstop) && (ogTime >= shiftlstart)){
+                        adjustedtime = shiftlstart;
+                        adjusttype = "Lunch Start";
+                    }
+
+
+                    else if ((ogTime <= shiftstop) && (ogTime >= gracestop )){
+                        adjustedtime = shiftstop;
+                        adjusttype = "Shift Stop";
+                    }
+
+                    else if ((ogTime >= Dockstop) && (ogTime < gracestop )){
+                        adjustedtime = Dockstop;
+                        adjusttype = "Shift Dock";
+                    }
+
+
                     else {
-                        punchDefaultAdjust(Intervalstart, punchTime);
+                        adjustedtime = -1;
+                        adjusttype = "Interval Round";
+                    }
+                    
+                break;
+                case 1:
+                    
+                    if ((ogTime > gracestart) && (ogTime <= Dockstart)){
+                        adjustedtime = Dockstart;
+                        adjusttype = "Shift Dock";
+                    }
+                    else if ((ogTime >= shiftstart) &&  (ogTime <= gracestart)){
+                         adjustedtime = shiftstart;
+                        adjusttype = "Shift Start";
+                    }
+                    else if  ((ogTime <= shiftlstop)  && (ogTime >= shiftlstart)){
+
+                        adjustedtime = shiftlstop;
+                        adjusttype = "Lunch Stop";
+                    }
+                    else if ((ogTime <= shiftstart)){
+
+                         adjustedtime = shiftstart;
+                        adjusttype = "Shift Start";
+                    }
+                    else {
+
+                        adjustedtime = -1;
+                        adjusttype = "Interval Round";
                     }
                 break;
+              }  
+
             }
-            /*
-            "Shift Start" and "Shift Stop": The employee’s early "clock in" punch or late "clock out" punch (within the Interval before the start of the shift or after the end of the shift) are realigned with the starting or stopping time of the shift, respectively.
-            
-            
-            "Lunch Start": The employee’s "clock out" punch (within the lunch break) is to be realigned with the start of lunch break.
-            
-            
-            "Lunch Stop": The employee’s "clock in" punch (within the lunch break) is to be realigned with the stop of lunch break.
-            
-            
-            "Grace Period": The employee’s late "clock in" punch or early "clock out" punch (within the Grace Period after the start of the shift or before the end of the shift) is to be realigned with the starting or stopping time of the shift, 
-            respectively.
-            
-            
-            "Dock": The employee’s late "clock in" punch (within the Dock interval after the start of the shift and outside the Grade Period) is to be adjusted forward in time from the start of the shift by this amount.  Similarly, the employee’s early "clock out" punch (within the Dock interval before the end of the shift and outside the Grade Period) is to be moved backward in time from the end of the shift by this amount.
-            
-            
-            "Interval Round": If an employee's punch falls outside any of the rules outlined above—that is, if it is outside the Intervals before the start of the shift and after the end of the shift, if it does not fall within the Grace or Dock periods at the start and end of the shift, and if it does not occur within the designated lunch break—it should simply be rounded up or down to the nearest increment of the Interval.  In addition to rounding the minutes up or down, the seconds should be set to zero in the adjusted timestamp.
-            
-            
-            "None": If the punch happens to have occurred at an even increment of the Interval (for example, if the Interval is set to 15 minutes and a first-shift employee clocks in at 9:30, disregarding the seconds), then no adjustment is necessary; all that needs to be done is to reset the seconds to zero.
-            */
-    }
+
+            else 
+            {
+                    
+                adjustedtime = -1;
+                adjusttype = "Interval Round";
+
+            }
+                  
+            makeAdjustment(s.getInterval() , originalpunch);
+                    
     }
 
-    /**
-     *
-     * @return
-     */
     public String getAdjustmenttype() {
         return adjustmenttype;
     }
@@ -256,12 +252,66 @@ public class Punch {
         return s;
     
     }
+    public String printAdjustedTimestamp(){
+         String s = "#";
+            String b = this.getBadgeid();
+            s += b;
 
-    private void setAdjustedTimestamp(long shiftlstart) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            switch (this.getPunchtypeid()){
+                case 0:
+                    s += " CLOCKED OUT: ";
+                    break;
+                case 1:
+                    s += " CLOCKED IN: ";
+                    break;
+                case 2:
+                    s += " TIMED OUT: ";
+            }
+
+            DateFormat df = new SimpleDateFormat("EEE MM/dd/yyyy HH:mm:ss");
+            Date d = new Date(this.adjustedtimestamp);
+
+            s += (df.format(d)).toUpperCase() + " (" + this.getAdjustmenttype()+ ")";
+
+            return s;
+    }
+    
+    private void setAdjustedTimestamp(long time) {
+        this.adjustedtimestamp = time;
     }
 
-    private void punchDefaultAdjust(long Intervalstart, long punchTime) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void makeAdjustment(int interval, GregorianCalendar originalpunch) 
+    {
+ 
+        int min = originalpunch.get(Calendar.MINUTE);
+        int rem = min % interval;
+        
+        if(adjusttype.equals("Interval Round"))
+        {
+            
+            if (rem != 0 )
+            {
+                if(rem < interval /2) 
+                {
+                    originalpunch.add(Calendar.MINUTE, - rem);
+                }
+                else
+                {
+                    originalpunch.add(Calendar.MINUTE, interval - rem);
+                }
+                adjustedtime = originalpunch.getTimeInMillis();
+            }
+            else
+            {
+                this.adjusttype = "None";
+                adjustedtime = originalpunch.getTimeInMillis();
+            }
+        }
+        
+        this.setAdjustedTimestamp(adjustedtime);
+        this.setAdjustmenttype(adjusttype);
+        
+        
+        
     }
 }
